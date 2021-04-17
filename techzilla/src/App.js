dca507f72f64dc4c2ae169af1029a00e14bb6608
Sagun Pandey <twiggymarilyn00@gmail.com>
2021-04-16 18:55:44 -0700

    logo added

--- techzilla/src/App.js
+++ techzilla/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.JPG';
+import logo from './assets/images/logo1.png';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>TECHZILLA.LLC</h1>
+    </header>
+  </div>
+
   );
 }
+const homePageData = {
+  overlapGroup7: "ellipse-10.svg",
+  eshop: "eshop.svg",
+  title: "Eshop",}
 
 export default App;
