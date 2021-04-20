37ee37c0886938d60ee1d431890885dfa62fa0c4
Sagun Pandey <twiggymarilyn00@gmail.com>
2021-04-20 15:51:21 -0700

    Update HeroSection.js

--- techzilla/src/components/HeroSection.js
+++ techzilla/src/components/HeroSection.js
@@ -6,7 +6,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
         <video src='video-2.mp4' autoPlay loop muted />
-      <h1>Welcocme to techZilla</h1>
+      <h1>Welcome to techZilla</h1>
       <p>Your trusted online Computer World</p>
     </div>
   );
