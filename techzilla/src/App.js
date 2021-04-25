ca314cac6a6dc26383e892c622fb921e43684744
Pierce Taylor <piercetaylor98@gmail.com>
2021-04-25 16:45:14 -0600

    navbar and login/signup page

--- techzilla/src/App.js
+++ techzilla/src/App.js
@@ -1,17 +1,28 @@
-//import logo from './components/assets/images/logo1.png';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Home from './components/homepage/Home';
+import Navbar from './components/Navbar'
+import Login from '../src/components/LoginSignUpPage/login.component'
+import SignUp from '../src/components/LoginSignUpPage/signup.component'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Router>
+    
+    
+        <Router>
+        <div className="App">
+        <Navbar/>
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/signup' exact component={SignUp} />
+          <Route path='/login' exact component={Login} />
         </Switch>
+        </div>
       </Router>
-    </>
+     
+    
   );
 }
 
---
+++ techzilla/src/components/Navbar.js
@@ -0,0 +1,58 @@
+import {React, useState} from 'react'
+import {Link} from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import './Navbar.css';
+ 
+//context API allows you to pass in properties \
+
+
+function Navbar() {
+
+    const [click, setclick] = useState(false);
+
+    const handleClick = () =>{
+        setclick(!click)
+    }
+    const closeMobileMenu = () => setclick(false);
+    return (
+       
+       <div className="navbar">
+           <div className="navbar-container container">
+            <Link
+             to='/' 
+             className="navbar-logo" 
+             onClick={closeMobileMenu}>
+                techZilla
+            </Link> 
+            <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars/>}
+            </div>
+            <ul className={click ? 'nav-menu active': 'nav-menu'}>
+             <li className="nav-item">
+                
+                <Link 
+                to='/login' 
+                className="nav-links" 
+                onClick={closeMobileMenu}>
+              
+                   Login
+                </Link>
+             </li>
+             <li className="nav-item">
+                <Link 
+                to='/signup' 
+                className="nav-links"
+                onClick={closeMobileMenu}>
+              
+                    SignUp
+                </Link>
+             </li>
+              
+            </ul>
+           </div>
+       </div>
+     
+    )
+}
+
+export default Navbar
