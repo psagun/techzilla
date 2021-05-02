57353272e401741bde33868049adee10868e1a3a
Sagun Pandey <twiggymarilyn00@gmail.com>
2021-05-01 17:29:27 -0700

    cards updated and product.js added
    cards updated and product.js added

--- techzilla/src/App.js
+++ techzilla/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Home from './components/homepage/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Products from './components/homepage/Products';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/products' component={Products} />
         </Switch>
       </Router>
     </>
