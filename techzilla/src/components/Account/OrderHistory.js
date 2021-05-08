ef2e9b37c79555094df02c27c841c2ce96ccf0e8
Soltis90 <Luigi.Otoya@gmail.com>
2021-05-08 02:37:18 -0400

    added footers and css for profile, order, payment

--- techzilla/src/components/Account/OrderHistory.js
+++ techzilla/src/components/Account/OrderHistory.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import Footer from '../footer/Footer';
+import './OrderHistory.css';
 
 class OrderHistory extends Component {
     render() {
         return(
-            <div className='order-container' style={{
-                margin: 'auto',
-                maxWidth: '1000px',
-                paddingTop: '25px',
-                borderBottomStyle:'solid',
-                borderWidth: 'thin'
-                }}>
-                <Link to='/Account'>Your Account</Link> <> {'> Your Orders'} </>
-                <h2>Your Orders</h2>
-                <div className='orders'>Orders
-                    <ul> list of orders here
+            <div className='order-page-container'>
+                <div className='order-container'>
+                    <Link to='/Account'>Your Account </Link> <> {'> Your Orders'} </>
+                    <h2>Your Orders</h2>
+                    <div className='orders'>Orders
+                        <ul> list of orders here
 
-                    </ul>
+                        </ul>
+                    </div>      
                 </div>
-            </div>            
+                <Footer/>
+            </div>
         )
     }
 }
--- techzilla/src/components/Account/PaymentMethods.js
+++ techzilla/src/components/Account/PaymentMethods.js
@@ -1,47 +1,42 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Footer from '../footer/Footer';
+import './PaymentMethods.css';
 
 class PaymentMethods extends Component {
     render() {
         return(
-            <div className='payment-container' style={{
-                margin: 'auto',
-                maxWidth: '1000px',
-                paddingTop: '25px',
-                borderBottomStyle:'solid',
-                borderWidth: 'thin'
-                }}>
-                <Link to='/Account'>Your Account</Link> <> {'> Payment Methods'} </>
-                <h2>Payment Methods</h2>
-                <div>
-                    <h4 style={{paddingTop:'15px'}}>Wallet</h4>
-                    <h5>Your credit and debit cards</h5>
-                    <div className='creditcards' style={{
-                        borderBottomStyle:'solid',
-                        borderWidth:'thin'
-                        }}>   
+            <div className='payment-page-container'>
+                <div className='payment-container'>
+                    <Link to='/Account'>Your Account</Link> <> {'> Payment Methods'} </>
+                    <h2>Payment Methods</h2>
+                    <div>
+                        <h4 style={{paddingTop:'15px'}}>Wallet</h4>
+                        <h5>Your credit and debit cards</h5>
+                        <div className='creditcards'>   
+                        </div>
+                        <h4>Add a New Payment Method</h4>
+                    <div className='addcards'>
+                        <form id='addcardform'>
+                            <div className='cardname'>
+                                <TextField label='Name' type='text' /> 
+                            </div>
+                            <div className='cardname'>
+                                <TextField label='Card Number' type='text'/>
+                            </div>
+                            <div className='cardname'>
+                                <TextField label='Expiration mm/yy' type='text'/>
+                            </div>
+                            <Button variant='dark' as="input" type="submit" value="Submit"/>
+                        </form>
                     </div>
-                    <h4>Add a New Payment Method</h4>
-                <div className='addcards'>
-                    <form id='addcardform'>
-                        <div className='cardname'>
-                            <TextField label='Name' type='text' /> 
-                        </div>
-                        <div className='cardname'>
-                            <TextField label='Card Number' type='text'/>
-                        </div>
-                        <div className='cardname'>
-                            <TextField label='Expiration mm/yy' type='text'/>
-                        </div>
-                        <Button variant='dark' as="input" type="submit" value="Submit"/>
-                    </form>
+                    </div>
                 </div>
-                </div>
+                <Footer/>
             </div>
         )
-
     }
 }
 
